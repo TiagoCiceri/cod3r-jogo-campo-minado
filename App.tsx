3d89c0d96b25f223eb2f7f6d4fca29f8e48de7d7
TiagoCiceri <tiagociceri@gmail.com>
2022-02-13 14:02:19 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react'
 import { StyleSheet, Text, View } from 'react-native';
+import params from './src/params'
+import MineField from './src/components/MineField'
+import { createMinedBoard } from './src/components/functions'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+//export default function App() {
+export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = this.createState()
+  }
+
+  minesAmount() {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState() {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+
+    return {
+      board: createMinedBoard(rows, cols, this.minesAmount()),
+    }
+
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Iniciando o Jogo MINES!!!</Text>
+
+        <Text>Tamanho da grade:
+          {params.getRowsAmount()} x {params.getColumnsAmount()}
+        </Text>
+
+        <View style={styles.board}>          
+          <MineField board={this.state.board}/>
+        </View>
+
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
   },
+  board: {
+    backgroundColor: '#AAA',
+    alignItems: 'center',
+  }
 });
